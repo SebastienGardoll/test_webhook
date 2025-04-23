f8f0bc75a0942c94443c6b6ab9fbdd192c197f81
Sébastien Gardoll <sebastien@gardoll.fr>
2025-04-23 17:34:39 +0200

    -a

--- update.py
+++ update.py
@@ -1,31 +1,77 @@
-from typing import Annotated
+from typing import Annotated, ReadableBuffer
 import hashlib
 import hmac
-from fastapi import APIRouter, HTTPException, Request, Header
+from fastapi import APIRouter, HTTPException, Request, Header, status
 import logging
 import json
 
-_LOGGER = logging.getLogger()
+BRANCH_NAME: str = 'esgvoc'
+_LOGGER = logging.getLogger('update')
 router = APIRouter()
 
 
-def verify_signature(payload_body, secret_token, signature_header):
-    """Verify that the payload was sent from GitHub by validating SHA256.
-
-    Raise and return 403 if not authorized.
-
-    Args:
-        payload_body: original request body to verify (request.body())
-        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
-        signature_header: header received from GitHub (x-hub-signature-256)
-    """
-    if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+def check_signature(raw_payload: ReadableBuffer | None,
+                    secret: str, signature: str | None) -> None:
+    if not signature:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='missing X-Hub-Signature-256 in the request header')
+    elif not raw_payload:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='missing body')
+    hash_object = hmac.new(secret.encode('utf-8'), msg=raw_payload, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
-    if not hmac.compare_digest(expected_signature, signature_header):
-        _LOGGER.warning('*** SIGNATURE DID NOT MATCH ***')
-        #raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+    if not hmac.compare_digest(expected_signature, signature):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Mismatch signature')
+
+
+def check_files(files: list[str]) -> bool:
+    result = False
+    for file in files:
+        pass  # TODO
+    return result
+
+
+def check_commit(commit: dict) -> bool:
+    if check_files(commit['modified']):
+        return True
+    elif check_files(commit['added']):
+        return True
+    elif check_files(commit['removed']):
+        return True
+    else:
+        return False
+
+
+def check_conditions(payload: dict, event_type: str | None) -> bool:
+    if event_type:
+        if event_type != 'push':
+            _LOGGER.info(f"ignored: event type is '{event_type}'")
+            return False
+    else:
+        msg = 'error: missing event_type'
+        _LOGGER.info(msg)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+    if 'ref' in payload:
+        if payload['ref'] != f'refs/heads/{BRANCH_NAME}':
+            _LOGGER.info(f"ignored: ref is '{payload['ref']}'")
+            return False
+    else:
+        msg = 'error: missing ref'
+        _LOGGER.info(msg)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+    if 'commits' in payload:
+        has_interesting_files = False
+        for commit in payload['commits']:
+            if check_commit(commit):
+                has_interesting_files = True
+                break
+        if not has_interesting_files:
+            _LOGGER.info('ignored: no interesting file were modified, added or deleted')
+            return False
+    else:
+        msg = 'error: missing commits'
+        _LOGGER.info(msg)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='missing commits')
+    return True
 
 
 @router.post('/update')
@@ -33,9 +79,7 @@ async def update(request: Request,
                  x_hub_signature_256: Annotated[str | None, Header()] = None,
                  x_github_event: Annotated[str | None, Header()] = None):
     raw_payload = await request.body()
-    verify_signature(payload_body=raw_payload, secret_token='monsecret', signature_header=x_hub_signature_256)
+    check_signature(raw_payload=raw_payload, secret='monsecret', signature=x_hub_signature_256)
     payload = json.loads(raw_payload)
-    _LOGGER.warning(f'event type: {x_github_event}')
-    _LOGGER.warning(f'ref: {payload.get("ref")}')
-    _LOGGER.warning(f'repository: {payload.get("repository")}')
-    _LOGGER.warning(f'commits: {payload.get("commits")}')
+    if check_conditions(payload, x_github_event):
+        pass
